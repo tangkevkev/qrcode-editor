63f0ae079f30bde32cd09ab8d198f950f4369d46
Kevin Tang <tangk@ethz.ch>
2021-08-23 01:10:07 +0200

    correct grid layout + non-functional download button

--- src/UI/QR-Editor.tsx
+++ src/UI/QR-Editor.tsx
@@ -1,6 +1,11 @@
 
 import { Grid } from '@material-ui/core';
 import QRCode from 'qrcode.react'
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import IconButton from '@material-ui/core/IconButton';
+import { Button } from '@material-ui/core';
+
 
 
 interface QRProps {
@@ -10,21 +15,49 @@ interface QRProps {
   subTitle?: string,
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    margin: {
+      margin: theme.spacing(1),
+    },
+    extendedIcon: {
+      marginRight: theme.spacing(1),
+    },
+  }),
+);
+
 function QREditor(props: QRProps) {
+
+  const classes = useStyles();
+
+
   return (
-    <Grid container spacing={0}
-      direction="column"
-      justifyContent="center"
-      alignItems="center">
-      <QRCode
-        size={256}
-        fgColor={props.color}
-        value={props.content}
-        level={'H'}
-      >
-      </QRCode>
-      {props.subTitle}
-    </Grid>
+    <div className="HpQrcode"> 
+
+      <Grid container spacing={0}
+        direction="column"
+        justifyContent="center"
+        alignItems="center">
+        <QRCode
+          size={256}
+          fgColor={props.color}
+          value={props.content}
+          level={'H'}
+          includeMargin={true}
+          imageSettings={{
+            src: "https://static.zpao.com/favicon.png"
+          }}
+        />
+        {props.subTitle}
+        <Button variant="text" color="primary" component="span">
+          Download
+          <IconButton aria-label="delete" className={classes.margin} size="small">
+            <ArrowDownwardIcon fontSize="inherit" />
+          </IconButton>
+        </Button>
+
+      </Grid>
+    </div>
   );
 }
 
--- src/UI/SingleEditor.tsx
+++ src/UI/SingleEditor.tsx
@@ -3,11 +3,10 @@ import { useState } from "react";
 import QREditor from './QR-Editor'
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { TwitterPicker, BlockPicker } from 'react-color'
+import { BlockPicker } from 'react-color'
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import { useTranslation } from 'react-i18next';
 
 
@@ -34,78 +33,76 @@ const useStyles = makeStyles((theme) => ({
 
 function SingleEditor() {
 
-    const classes = useStyles();
-    const { t, } = useTranslation();
-  
-    const DEFAULT_IMAGE_SRC = ""
-    const DEFAULT_USE_IMAGE = false
-    const DEFAULT_CONTENT = ""
-    const DEFAULT_COLOR = ""
-  
-    const [imageSrc, setImageSrc] = useState("")
-    const [useImage, setUseImage] = useState(false)
-    const [content, setContent] = useState("google.ch")
-    const [subTitle, setSubtitle] = useState("")
-    const [color, setColor] = useState("#00FF00")
-  
-  
-    function reset() {
-      setImageSrc(DEFAULT_IMAGE_SRC)
-      setUseImage(DEFAULT_USE_IMAGE)
-      setContent(DEFAULT_CONTENT)
-      setColor(DEFAULT_COLOR)
-    }
-  
-  
-    return (
-      <div className={`${classes.rootGrid}`}>
-        <Grid container spacing={2}
-          direction="row">
-          <Grid item xs={12} lg={4}  />
-          <Grid item xs={12} lg={2} 
-            container
-            direction="column"
-            justifyContent="flex-end"
-            alignItems="center">
-            <Paper elevation={3}>
-  
-              <Grid item>
-                <form className={classes.root} noValidate autoComplete="off">
-                  <TextField id="standard-basic"
-                    label="QR Content"
-                    onChange={(event) => { setContent(event.target.value) }}
-                    value={content}
-                  />
-                </form>
-              </Grid>
-              <Grid item>
-                <form className={classes.root} noValidate autoComplete="off">
-                  <TextField id="standard-basic"
-                    label="Subtitle"
-                    onChange={(event) => { setSubtitle(event.target.value) }}
-                    value={subTitle}
-                  />
-                </form>
-              </Grid>
-              <Grid item
-                container
-                justifyContent="center"
-                alignItems="center">
-                <BlockPicker color={color} onChange={(event) => setColor(event.hex)} triangle={'hide'} />
-              </Grid>
-            </Paper>
-  
-          </Grid>
-  
-          <Grid item xs={12} lg={2} 
-            container
-            alignItems="center"
-            justifyContent="flex-start">
-            <QREditor content={content} color={color} imageSrc={imageSrc} subTitle={subTitle} />
-          </Grid>
-        </Grid>
-  
-      </div>
-    );
+  const classes = useStyles();
+  const { t, } = useTranslation();
+
+  const DEFAULT_IMAGE_SRC = ""
+  const DEFAULT_USE_IMAGE = false
+  const DEFAULT_CONTENT = ""
+  const DEFAULT_COLOR = ""
+
+  const [imageSrc, setImageSrc] = useState("")
+  const [useImage, setUseImage] = useState(false)
+  const [content, setContent] = useState("google.ch")
+  const [subTitle, setSubtitle] = useState("")
+  const [color, setColor] = useState("#000000")
+
+
+  function reset() {
+    setImageSrc(DEFAULT_IMAGE_SRC)
+    setUseImage(DEFAULT_USE_IMAGE)
+    setContent(DEFAULT_CONTENT)
+    setColor(DEFAULT_COLOR)
   }
-  export default SingleEditor;
+
+
+  return (
+    <div className={`${classes.rootGrid}`}>
+      <Grid container spacing={2}
+        direction="row"
+        alignContent="center"
+        justify="center">
+
+        <Paper elevation={4}>
+
+
+          <Grid item>
+            <form className={classes.root} noValidate autoComplete="off">
+              <TextField id="standard-basic"
+                multiline={true}
+                maxRows={3}
+                label="QR Content"
+                onChange={(event) => { setContent(event.target.value) }}
+                value={content}
+              />
+            </form>
+            <form className={classes.root} noValidate autoComplete="off">
+              <TextField id="standard-basic"
+                label="Subtitle [Optional]"
+                onChange={(event) => { setSubtitle(event.target.value) }}
+                value={subTitle}
+              />
+            </form>
+            <div className="flex-container">
+              <BlockPicker
+                color={color}
+                colors={['#000000', '#808080', '#FF0000',
+                  '#FFD800', '#4CFF00', '#00FFFF',
+                  '#0094FF', '#0026FF', '#FF7FED', '#007F0E']}
+                onChange={(event) => setColor(event.hex)} triangle={'hide'} />
+            </div>
+          </Grid>
+        </Paper>
+
+
+
+        <Grid item>
+          <QREditor content={content} color={color} imageSrc={imageSrc} subTitle={subTitle} />
+        </Grid>
+
+      </Grid>
+
+    </div>
+  );
+}
+export default SingleEditor;
